bd8b0a803aed39213bcab784d3d4dce99fd7476d
Felipe Junges <felipeejunges@hotmail.com>
2023-10-05 20:27:53 -0300

    reschedule and fix sidekiq (#20)

---
+++ app/sidekiq/reschedule_invoice_with_error_job.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class RescheduleInvoiceWithErrorJob < ApplicationJob
+  def perform(*_args)
+    Client::Invoice.where(status: :error).each do |invoice|
+      GenerateInvoiceJob.perform_in(1, { 'client_id': invoice.client_id, date: invoice.reference_date }.to_json)
+    end
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module TesteRubyFelipe
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.time_zone = 'America/Sao_Paulo'
   end
 end
